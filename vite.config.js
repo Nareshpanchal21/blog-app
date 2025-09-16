50815e30ca00fcd32763c78d0b249cb6ed4a0c80
Naresh Panchal <nareshpanchal2151@gmail.com>
2025-09-16 10:42:53 -0700

    Build ready for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/blog-app/',  // ← aapke repo name ke hisab se
   plugins: [react()],
-})
+  base: '/', // Agar app root pe host ho rahi hai (Render default)
+});
